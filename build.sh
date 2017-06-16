10b24ad3b276a57b74e49c80a28d3d5b78f9a2c6
Ludovic Claude <ludovic.claude@laposte.net>
2017-06-17 00:06:31 +0200

    Use CircleCI 2.0

--- build.sh
+++ build.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
-if groups $USER | grep &>/dev/null '\bdocker\b'; then
+if pgrep -lf sshuttle > /dev/null ; then
+  echo "sshuttle detected. Please close this program as it messes with networking and prevents builds inside Docker to work"
+  exit 1
+fi
+
+if [ "$CIRCLECI" = true ] || groups $USER | grep &>/dev/null '\bdocker\b'; then
   CAPTAIN="captain"
 else
   CAPTAIN="sudo captain"
 fi
 
-BUILD_DATE=$(date --iso-8601=seconds) \
+BUILD_DATE=$(date -Iseconds) \
   VCS_REF=$(git describe --tags --dirty) \
   VERSION=$(git describe --tags --dirty) \
   $CAPTAIN build
